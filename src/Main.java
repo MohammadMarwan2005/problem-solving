cfbe2096e74dd1f986a411aa08f801c22e33e7be
Mohammad Al-Aishat <alaishatmohammad@gmail.com>
2025-07-12 13:07:53 +0300

    🚀 Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Mohammad Al-Aishat on 7/12/2025.
+ * Problem-Solving Project.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello problems, we'll find your solutions!");
+    }
+}
